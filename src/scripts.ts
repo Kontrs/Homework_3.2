69ed535e422bf168d56405d063390898c22969bb
Kristers Pulle <pulle265@gmail.com>
2023-11-07 17:42:01 +0200

    Created hamburger menu for mobile devices

--- src/scripts.ts
+++ src/scripts.ts
@@ -6,3 +6,16 @@ console.log('Ready for coding');
 console.log('Body jQuery node:', $('body'));
 console.log('Body javascript node:', document.querySelector('body'));
 console.log('2 + 3 =', sum(2, 3));
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+})
+
+document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
